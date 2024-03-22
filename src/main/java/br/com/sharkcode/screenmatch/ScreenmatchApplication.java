6326102857ad2c31fd842faf88c1ee3fe4d60d68
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-03-22 12:50:05 -0300

    Criando conversor de json

--- src/main/java/br/com/sharkcode/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/sharkcode/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.sharkcode.screenmatch;
 
+import br.com.sharkcode.screenmatch.models.DadosSerie;
 import br.com.sharkcode.screenmatch.services.ConsumoApi;
+import br.com.sharkcode.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +10,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=230c22ac");
-		System.out.println(json);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=230c22ac");
+        System.out.println(json);
+
+        ConverteDados conversor = new ConverteDados();
+
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+
+        System.out.println(dados);
+    }
 }
