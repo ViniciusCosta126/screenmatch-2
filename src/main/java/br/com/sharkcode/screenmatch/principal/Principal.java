ceae771ad53b6983aba1255ed7f2c1acd9dde7f4
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-03-26 15:44:46 -0300

    Criacao da classe episodio e formantando os dados de cada episodio para dentro da classe

--- src/main/java/br/com/sharkcode/screenmatch/principal/Principal.java
+++ src/main/java/br/com/sharkcode/screenmatch/principal/Principal.java
@@ -1,13 +1,17 @@
 package br.com.sharkcode.screenmatch.principal;
 
+import br.com.sharkcode.screenmatch.models.DadosEpisodio;
 import br.com.sharkcode.screenmatch.models.DadosSerie;
 import br.com.sharkcode.screenmatch.models.DadosTemporada;
+import br.com.sharkcode.screenmatch.models.Episodio;
 import br.com.sharkcode.screenmatch.services.ConsumoApi;
 import br.com.sharkcode.screenmatch.services.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -33,5 +37,25 @@ public class Principal {
             temporadas.add(dadosTemporada);
         }
         temporadas.forEach(System.out::println);
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titutlo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t-> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\n Top 5 episodios");
+        dadosEpisodios.stream()
+                .filter(e-> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t->t.episodios().stream()
+                        .map(d -> new Episodio(t.temporada(), d)))
+                .collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
     }
 }
